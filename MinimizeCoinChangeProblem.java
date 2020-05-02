6abb5e6fe33848b24a0f578e62afefc2d87566f6
vritansh <vritansh14@gmail.com>
2020-05-03 02:29:20 +0530

    updating greedy-algorithms

--- MinimizeCoinChangeProblem.java
+++ MinimizeCoinChangeProblem.java
@@ -4,7 +4,7 @@ static int findMaximum (int V)
     int[] arr = {1000,500,100,50,20,10,5,2,1};
     int     n = arr.length;
     for(int i =0 ; i<n;   i++)
-            if(V>=arr[i])
+            if(V>arr[i])
             {
                    return arr[i];
               
@@ -13,7 +13,7 @@ static int findMaximum (int V)
    }    
 public static void main(String[] args)
 { 
-    int V = 121;
+    int V = 1023;
     int l ; 
     int count = 0 ;
     while(V!=0)
